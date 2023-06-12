8b3530448b1b725c0fd7736a3b1a3fda4e617895
IBaixauliCesumin <135705920+IBaixauliCesumin@users.noreply.github.com>
2023-06-12 13:01:37 +0200

    Create __manifest__.py

---
+++ cesumin/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Cesumin",
+    'version': '16.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'author': "Formacion Cesumin",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/my_model_views.xml',
+    ],
+}
