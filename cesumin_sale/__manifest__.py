db9466034b305fa9c9dda5b2e35e6661aa2e2aeb
Paco <pacococacruz@gmail.com>
2023-06-13 12:47:05 +0200

    [ADD]cesumin_sale: override record

---
+++ cesumin_sale/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Cesumin Sale",
+    'summary': "Cesumin Sale",
+    'version': '16.0.1.0.0',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+}
