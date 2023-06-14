ef842726ac2fc604e0ad4d9c27b3c686c25ea04c
Paco <pacococacruz@gmail.com>
2023-06-14 13:02:04 +0200

    [IMP]cesumin_apt: modify report

--- cesumin_sale/models/sale_order.py
+++ cesumin_sale/models/sale_order.py
@@ -4,6 +4,8 @@ from odoo import api, models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    # CRUD metodos, tienen que sobreescribirse muchas veces
+    # create, write, unlink (mejor a traves de @api.ondelete()), read
     example = fields.Char()
 
     def action_cancel(self):
