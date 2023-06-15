825e19db1cff707356d5a9d6c2e3e57b5a1351d1
Paco <pacococacruz@gmail.com>
2023-06-15 11:45:38 +0200

    [IMP]cesumint_sale: wizard second part

--- cesumin_sale/__manifest__.py
+++ cesumin_sale/__manifest__.py
@@ -6,9 +6,10 @@
         'sale',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/res_partner_views.xml',
         'views/sale_order_views.xml',
         'reports/sale_order_templates.xml',
-        
+        'wizards/create_sale_order_views.xml',
     ],
 }
