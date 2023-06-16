c1d6e2073c13af52d7cdf96cb8370d56a1f3d8c6
Paco <pacococacruz@gmail.com>
2023-06-16 09:35:40 +0200

    [ADD]cesumint_sass: sass example

---
+++ cesumin_sass/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Cesumin Sass",
+    'summary': "Cesumin Sass",
+    'version': '16.0.1.0.0',
+    'depends': [
+        'cesumin_apt',
+    ],
+    'data': [
+        'models/amazon_seller.xml',
+    ],
+}
